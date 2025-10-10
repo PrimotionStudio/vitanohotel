1a3a4b3585adedc67d0b6e5689e1c811ce1cfb6f
Primotion Studio <oyedelenewton@gmail.com>
2025-10-10 20:03:56 +0100

    Refactor: Update image paths in app.json and index.tsx
    Updated image paths to reflect the new directory structure. Added more
    content to the landing page.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,16 +3,16 @@ import {
   ImageBackground,
   Platform,
   Pressable,
-  SafeAreaView,
   ScrollView,
   Text,
   View,
 } from "react-native";
 import Logo from "@/assets/images/logo.png";
 import LandingPage1 from "@/assets/images/landing-page/1.jpg";
+import LandingPage5 from "@/assets/images/landing-page/5.jpg";
+import LandingPage6 from "@/assets/images/landing-page/6.jpg";
 import { useState } from "react";
 import DateTimePicker from "@react-native-community/datetimepicker";
-import { SafeAreaContext } from "react-native-safe-area-context";
 import { SimpleBookingType } from "@/utils/zod";
 import { ArrowDown, ChevronDown } from "lucide-react-native";
 
@@ -44,9 +44,9 @@ export default function LandingScreen() {
   };
 
   return (
-    <ScrollView className="flex-1">
+    <ScrollView className="bg-white">
       <ImageBackground
-        className="object-cover flex gap-y-36 items-center justify-center pb-36"
+        className="object-cover flex gap-y-32 items-center justify-center pb-36 bg-white"
         resizeMode="cover"
         source={LandingPage1}
       >
@@ -126,12 +126,99 @@ export default function LandingScreen() {
         </View>
       </ImageBackground>
 
-      <View className="bg-white h-[50vh] px-5 py-10">
+      <View className="bg-white px-5 py-10">
         <Text className="text-3xl text-center">
           WHERE ELEGANCE MEETS EXCELLENCE
         </Text>
 
-        <View className="relative h-[40vh"></View>
+        <View className="h-[40vh] my-12 flex items-center">
+          <Image
+            source={LandingPage5}
+            width={50}
+            height={50}
+            className="w-full h-2/3"
+            resizeMode="contain"
+          />
+          <Image
+            source={LandingPage6}
+            width={50}
+            height={50}
+            className="w-[150px] h-[150px] absolute bottom-10 self-end"
+            resizeMode="contain"
+          />
+        </View>
+        <View className="gap-5">
+          <Text className="text-[#ab916c]">
+            DISCOVER WHERE TO STAY IN LAGOS
+          </Text>
+          <Text className="text-4xl">Luxury hotel in Lagos</Text>
+          <Text className="text-lg">
+            Vintano presents a fresh perspective on luxury, beautiful rooms and
+            a comprehensive range of amenities. Located in Lagos, Nigeria, it
+            delivers a redefined level of service.
+          </Text>
+
+          <Pressable className="text-white w-[130px] border border-[#ab916c] hover:bg-[#ab916c] px-6 py-4 group">
+            <Text className="text-lg text-center group-hover:text-white">
+              Take A Tour
+            </Text>
+          </Pressable>
+        </View>
+      </View>
+
+      <View className="bg-white py-10 flex gap-10">
+        <View className="gap-5">
+          <Text className="text-[#ab916c] text-center">
+            EXTRAORDINARY ACCOMODATIONS
+          </Text>
+          <Text className="text-3xl text-center">
+            Choose Your Apartment Type
+          </Text>
+        </View>
+        <View className="flex h-[250px] items-center">
+          <Image
+            source={LandingPage5}
+            width={50}
+            height={50}
+            className="h-full"
+            resizeMode="contain"
+          />
+          <View className="absolute bottom-10 left-0 bg-black px-[3rem] py-3">
+            <Text className="text-white text-2xl">Vitano Classic</Text>
+          </View>
+        </View>
+
+        <View className="flex h-[250px] items-center">
+          <Image
+            source={LandingPage5}
+            width={50}
+            height={50}
+            className="h-full"
+            resizeMode="contain"
+          />
+          <View className="absolute bottom-10 left-0 bg-black px-[3rem] py-3">
+            <Text className="text-white text-2xl">Vitano Twin</Text>
+          </View>
+        </View>
+
+        <View className="flex h-[250px] items-center">
+          <Image
+            source={LandingPage5}
+            width={50}
+            height={50}
+            className="h-full"
+            resizeMode="contain"
+          />
+          <View className="absolute bottom-10 left-0 bg-black px-[3rem] py-3">
+            <Text className="text-white text-2xl">Vitano Executive</Text>
+          </View>
+        </View>
+
+        <View className="items-center">
+          <Pressable className="text-white bg-[#ab916c] px-6 py-4">
+            <Text className="text-xl text-white">Visit More Rooms</Text>
+          </Pressable>
+        </View>
       </View>
     </ScrollView>
   );
